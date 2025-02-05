307c78960373fe1ebcd73e1d26cbc57a8a552574
ad1005772 <ad1005772@otc.edu>
2025-02-04 19:14:49 -0600

    PartTimeEmployee

---
+++ EmployeeManagement/EmployeeManagement/PartTimeEmployee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement
+{
+    internal class PartTimeEmployee
+    {
+        public class PartTimeEmployee : Employee
+        {
+            public decimal hourlyRate { get; set; }
+            public int hoursWorked {  get; set; }
+
+            public PartTimeEmployee(int ID, string Name, string Department, decimal BaseSalary, decimal HourlyRate, int HoursWorked) : base(ID, Name, Department, BaseSalary)
+            {
+                hourlyRate = HourlyRate;
+                hoursWorked = HoursWorked;
+            }
+            public override decimal CalculatePay()
+            {
+                return hourlyRate * hoursWorked;
+            }
+
+            public override void DisplayEmployeeDetails()
+            {
+                Console.WriteLine($"ID: {id}, Name: {name}, Department = {department}, Base Salary: {baseSalary}, Hourly Rate: {hourlyRate}, Hours Worked: {hoursWorked}");
+            }
+        }
+}
--- EmployeeManagement/EmployeeManagement/obj/Debug/net8.0/EmployeeManagement.AssemblyInfo.cs
+++ EmployeeManagement/EmployeeManagement/obj/Debug/net8.0/EmployeeManagement.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmployeeManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0bee82d2e6b1fa4fede635ce1c505f447903bbd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+348c5959fee32a918c637ae551f872cd12781ee3")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmployeeManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmployeeManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
